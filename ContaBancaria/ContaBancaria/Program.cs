2b3c50f1994877dd8fa2d14c855b024d6d383164
JonatasFO <jonatas_098@hotmail.com>
2019-04-09 14:44:18 -0300

    commit inicial - ContaBancaria

---
+++ ContaBancaria/ContaBancaria/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+
+namespace Secao5_aula60 {
+    class Program {
+        static void Main(string[] args) {
+
+            Conta conta;
+
+            Console.Write("Entre o numero da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre o titular da conta: ");
+            string titular = Console.ReadLine();
+
+            Console.Write("Havera deposito inicial (s/n)? ");
+            char resp = char.Parse(Console.ReadLine());
+            if (resp == 's' || resp == 'S') {
+
+                Console.Write("Entre o valor de deposito inicial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                conta = new Conta(numero, titular, depositoInicial);
+
+            }
+            else {
+                conta = new Conta(numero, titular);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para deposito: ");
+            double depos = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(depos);
+            Console.WriteLine("Dados atualizados: ");
+            Console.WriteLine(conta);
+
+            Console.WriteLine();
+            Console.Write("Entre um valor para saque: ");
+            double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(saque);
+            Console.WriteLine("Dados atualizados: ");
+            Console.WriteLine(conta);
+
+        }
+    }
+}
